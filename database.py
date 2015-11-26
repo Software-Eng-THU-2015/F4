61c1cd3c7e6c9da9f91cab89871cd0df67f652ad
ParallelMk2 <caijiaming2013@gmail.com>
2015-11-26 19:32:05 +0800

    database更新&测试

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import web
 import sae
+import datetime
 
 class database:
 	def __init__(self):
@@ -14,79 +15,217 @@ class database:
 	
 		self.message = message(self.db)
 		self.user = user(self.db)
-		self.sport = sport(self.db)
+		self.bong = bong(self.db)
+		self.follower = follower(self.db)
+		self.stranger = stranger(self.db)
 
 class message:
 	def __init__(self, db):
 		self.db = db
 
 	def get_token(self):
-		return self.db.query("select * from Message where Type = 'TOKEN'")[0].Text
+		try:
+			reply = self.db.query("select * from Message where Type = 'TOKEN'")[0].Text
+		except:
+			reply = ""
+		return reply
 
 	def get_appid(self):
-		return self.db.query("select * from Message where Type = 'APPID'")[0].Text
+		try:
+			reply = self.db.query("select * from Message where Type = 'APPID'")[0].Text
+		except:
+			reply = ""
+		return reply
 
 	def get_appsecret(self):
-		return self.db.query("select * from Message where Type = 'APPSECRET'")[0].Text
+		try:
+			reply = self.db.query("select * from Message where Type = 'APPSECRET'")[0].Text
+		except:
+			reply = ""
+		return reply
 
 	def update_token(self, accessToken):
-		if not accessToken:
-			return 0
-		self.db.update("Message", where = "id = 1", Token = accessToken)
-		return 1
+		return self.db.update("Message", where = "id = 1", Text = accessToken)
 
 class user:
 	def __init__(self, db):
 		self.db = db
 
-	def insert(self, uname, openid, imgurl):
-		if not openid:
-			return 0
-
-		if not uname:
-			uname = "anonymous"
-		if not imgurl:
-			imgurl = "http://fb.topit.me/b/3c/6c/11480604151c06c3cbl.jpg"
-
-		self.db.insert('User', Uname = uname, OpenID = openid, ImageUrl = imgurl)
-		return 1
+	def insert(self, openid, uname = "anonymous", headimgurl = "http://fb.topit.me/b/3c/6c/11480604151c06c3cbl.jpg"):
+		return self.db.insert("User", Uname = uname, OpenID = openid, ImageUrl = headimgurl)
 
 	def delete(self, openid):
-		if not openid:
-			return 0
-		self.db.delete("User", where = "OpenID = $id", vars = {"id": openid, })
-		return 1
+		return self.db.delete("User", where = "OpenID = $id", vars = {"id": openid, })
 
-	def update(self, openid, uname, imgurl):
-		if uname and imgurl:
-			self.db.update("User", where = "OpenID = $id", vars = {"id": openid, }, Uname = uname, ImageUrl = imgurl)
-			return
+	def update(self, openid, uname = "", headimgurl = ""):
+		if uname and headimgurl:
+			return self.db.update("User", where = "OpenID = $id", vars = {"id": openid, }, Uname = uname, ImageUrl = headimgurl)
 		if uname:
-			self.db.update("User", where = "OpenID = $id", vars = {"id": openid, }, Uname = uname)
-			return
-		if imgurl:
-			self.db.update("User", where = "OpenID = $id", vars = {"id": openid, }, ImageUrl = imgurl)
-			return
+			return self.db.update("User", where = "OpenID = $id", vars = {"id": openid, }, Uname = uname)
+		if headimgurl:
+			return self.db.update("User", where = "OpenID = $id", vars = {"id": openid, }, ImageUrl = headimgurl)
+		return -1
 
-	def get(self, openid):
-		if not openid:
-			return 0
-		return self.db.query("select * from User where OpenID = $id", vars = {"id": openid, })[0]
-
-class sport:
+	def get(self, openid = "", id = ""):
+		if not (openid or id):
+			return {}
+		try:
+			if openid:
+				user = self.db.query("select * from User where OpenID = $id", vars = {"id": openid, })[0]
+			elif id:
+				user = self.db.query("select * from User where id = $id", vars = {"id": id, })[0]
+			userdic = {}
+			userdic["openid"] = user.OpenID
+			userdic["id"] = user.id
+			userdic["nickname"] = user.Uname
+			userdic["headimgurl"] = user.ImageUrl
+		except:
+			userdic = {}
+		return userdic
+		
+class bong:
 	def __init__(self, db):
 		self.db = db
+		self.data_type_1 = ["openid", "startTime", "endTime", "type", "subType"]
+		self.data_type_2 = ["distance", "speed", "calories", "steps", "actTime", "nonActTime", "dsNum", "lsNum", "wakeNum", "wakeTimes", "score"]
 
-	def insert(self):
-		pass
+	'''def insert(self, openid, start_time, end_time, type, subtype, distance = 0, speed = 0, calories = 0, steps = 0, acttime = 0, nonacttime = 0, dsnum = 0, lsnum = 0, wakenum = 0, waketimes = 0, score = 0):
+		if type not in range(1, 4):
+			return -1
+		if subtype not in range(1, 7):
+			return -1
+		return self.db.insert("Bong", 
+			OpenID = openid, 
+			StartTime = start_time, 
+			EndTime = end_time, 
+			Type = type, 
+			SubType = subtype, 
+			Distance = distance, 
+			Speed = speed, 
+			Calories = calories, 
+			Steps = steps, 
+			ActTime = acttime, 
+			NonActTime = nonacttime, 
+			DsNum = dsnum, 
+			LsNum = lsnum, 
+			WakeNum = wakenum, 
+			WakeTimes = waketimes, 
+			Score = score)'''
 
-	def delete(self):
-		pass
+	def insert(self, data):
+		for index in self.data_type_1:
+			if index not in data:
+				return -1
+		if data["type"] not in range(1, 4):
+			return -1
+		if data["subType"] not in range(1, 7):
+			return -1
+		for index in self.data_type_2:
+			if index not in data:
+				data[index] = 0
+		return self.db.insert("Bong", 
+			OpenID = data["openid"], 
+			StartTime = data["startTime"], 
+			EndTime = data["endTime"], 
+			Type = data["type"], 
+			SubType = data["subType"], 
+			Distance = data["distance"], 
+			Speed = data["speed"], 
+			Calories = data["calories"], 
+			Steps = data["steps"], 
+			ActTime = data["actTime"], 
+			NonActTime = data["nonActTime"], 
+			DsNum = data["dsNum"], 
+			LsNum = data["lsNum"], 
+			WakeNum = data["wakeNum"], 
+			WakeTimes = data["wakeTimes"], 
+			Score = data["score"])
+
+	def delete(self, openid):
+		return self.db.delete("Bong", where = "OpenID = $id", vars = {"id": openid, })
 
 	def update(self):
 		pass
 
-	def get(self, openid):
-		if not openid:
+	def get_calories(self, openid, date):
+		date_1 = date
+		date_2 = datetime.datetime.strptime(date, "%Y-%m-%d") + datetime.timedelta(days = 1)
+		date_2 = date_2.strftime("%Y-%m-%d")
+		list = self.db.query("select * from Bong where StartTime >= $date_1 and StartTime < $date_2", vars = {"date_1": date_1, "date_2": date_2, })
+		calories = 0
+		try:
+			for index in list:
+				calories += index.Calories
+		except:
+			calories = 0
+		return calories
+
+	def get_sleep(self, openid, date):
+		date_1 = date
+		date_2 = datetime.datetime.strptime(date, "%Y-%m-%d") + datetime.timedelta(days = 1)
+		date_2 = date_2.strftime("%Y-%m-%d")
+		list = self.db.query("select * from Bong where EndTime >= $date_1 and EndTime < $date_2 and Type = 1", vars = {"date_1": date_1, "date_2": date_2, })
+		dsleep = 0
+		lsleep = 0
+		try:
+			for index in list:
+				dsleep += index.DsNum
+				lsleep += index.LsNum
+		except:
+			dsleep = 0
+			lsleep = 0
+		return {"dsleep": dsleep, "lsleep": lsleep}
+
+class follower:
+	def __init__(self, db):
+		self.db = db
+
+	def insert(self, follower_a, follower_b):
+		reply_a = self.db.insert("Follower", FollowerID = follower_a, FollowingID = follower_b)
+		reply_b = self.db.insert("Follower", FollowerID = follower_b, FollowingID = follower_a)
+		return reply_a and reply_b
+
+	def delete(self, follower_a, follower_b):
+		reply_a = self.db.delete("Follower", where = "FollowerID = $id_a and FollowingID = $id_b", vars = {"id_a": follower_a, "id_b": follower_b, })
+		reply_b = self.db.delete("Follower", where = "FollowerID = $id_a and FollowingID = $id_b", vars = {"id_a": follower_b, "id_b": follower_a, })
+		return reply_a and reply_b
+
+	def update(self):
+		pass
+
+	def get(self, follower):
+		follower_list = self.db.query("select * from Follower where FollowerID = $id", vars = {"id": follower, })
+		reply_list = []
+		for index in follower_list:
+			reply = index.FollowingID
+			reply_list.append(reply)
+		return reply_list
+
+	def is_followed(self, follower_a, follower_b):
+		reply = self.db.query("select * from Follower where FollowerID = $id_a and FollowingID = $id_b", vars = {"id_a": follower_a, "id_b": follower_b})
+		try:
+			tmp = reply[0]
+			return 1
+		except:
 			return 0
-		return self.db.query("select * from Sport where UserID = $id", vars = {"id": openid, })
+
+class stranger:
+	def __init__(self, db):
+		self.db = db
+
+	def insert(self, to_user, from_user):
+		return self.db.insert("Stranger", ToUserID = to_user, FromUserID = from_user)
+
+	def delete(self, to_user, from_user):
+		return self.db.delete("Stranger", where = "ToUserID = $id_a and FromUserID = $id_b", vars = {"id_a": to_user, "id_b": from_user, })
+
+	def update(self):
+		pass
+
+	def get(self, to_user):
+		stranger_list = self.db.query("select * from Stranger where ToUserID = $id", vars = {"id": to_user, })
+		reply_list = []
+		for index in stranger_list:
+			reply = index.FromUserID
+			reply_list.append(reply)
+		return reply_list
---
+++ test/test_database.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+import unittest
+import database
+
+class test_database_message(unittest.TestCase):
+	def setUp(self):
+		self.test_class = database.database()
+		self.test_token = "test_token"
+		
+	def tearDown(self):
+		pass
+
+	def test_message_get_token(self):
+		self.assertNotEqual(self.test_class.message.get_token(), "")
+
+	def test_message_get_appid(self):
+		self.assertNotEqual(self.test_class.message.get_appid(), "")
+
+	def test_message_get_appsecret(self):
+		self.assertNotEqual(self.test_class.message.get_appsecret(), "")
+
+	def test_message_update_token(self):
+		self.test_class.message.update_token(self.test_token)
+		self.assertEqual(self.test_class.message.get_token(), self.test_token)
+
+class test_database_user(unittest.TestCase):
+	def setUp(self):
+		self.test_class = database.database()
+		self.test_user = {"openid": "testuser", "nickname": "username", "headimgurl": "http://baidu.com"}
+		self.test_new_user = {"openid": "testuser", "nickname": "new_username", "headimgurl": "new_headimgurl"}
+
+	def tearDown(self):
+		pass
+
+	def test_user_insert_get(self):
+		self.test_class.user.insert(openid = self.test_user["openid"], uname = self.test_user["nickname"], headimgurl = self.test_user["headimgurl"])
+		#通过openid查询
+		query_user_openid = self.test_class.user.get(openid = self.test_user["openid"])
+		self.test_user["id"] = query_user_openid["id"]
+		self.assertEqual(query_user_openid, self.test_user)
+		#通过id查询
+		query_user_id = self.test_class.user.get(id = query_user_openid["id"])
+		self.assertEqual(query_user_id, self.test_user)
+
+		self.test_class.user.delete(openid = self.test_user["openid"])
+
+	def test_user_update(self):
+		self.test_class.user.insert(openid = self.test_user["openid"], uname = self.test_user["nickname"], headimgurl = self.test_user["headimgurl"])
+		#同时更新uname和headimgurl
+		self.test_class.user.update(openid = self.test_user["openid"], uname = self.test_new_user["nickname"], headimgurl = self.test_new_user["headimgurl"])
+		query_user = self.test_class.user.get(openid = self.test_user["openid"])
+		self.test_new_user["id"] = query_user["id"]
+		self.assertEqual(query_user, self.test_new_user)
+		#仅更新uname
+		self.test_class.user.update(openid = self.test_user["openid"], uname = self.test_user["nickname"])
+		self.assertEqual(self.test_class.user.get(openid = self.test_user["openid"])["nickname"], self.test_user["nickname"])
+		#仅更新headimgurl
+		self.test_class.user.update(openid = self.test_user["openid"], headimgurl = self.test_user["headimgurl"])
+		self.assertEqual(self.test_class.user.get(openid = self.test_user["openid"])["headimgurl"], self.test_user["headimgurl"])
+
+		self.test_class.user.delete(openid = self.test_user["openid"])
+
+	def test_user_delete(self):
+		self.test_class.user.insert(openid = self.test_user["openid"], uname = self.test_user["nickname"], headimgurl = self.test_user["headimgurl"])
+		self.test_class.user.delete(openid = self.test_user["openid"])
+		self.assertEqual(self.test_class.user.get(openid = self.test_user["openid"]), {})
+
+class test_database_follower(unittest.TestCase):
+	def setUp(self):
+		self.test_class = database.database()
+		self.follower_a = -10000
+		self.follower_b = -10001
+
+	def tearDown(self):
+		pass
+
+	def test_follower_insert_get(self):
+		self.test_class.follower.insert(self.follower_a, self.follower_b)
+		self.assertIn(self.follower_b, self.test_class.follower.get(self.follower_a))
+		self.assertIn(self.follower_a, self.test_class.follower.get(self.follower_b))
+		self.test_class.follower.delete(self.follower_a, self.follower_b)
+
+	def test_follower_delete(self):
+		self.test_class.follower.insert(self.follower_a, self.follower_b)
+		self.test_class.follower.delete(self.follower_a, self.follower_b)
+		self.assertNotIn(self.follower_b, self.test_class.follower.get(self.follower_a))
+		self.assertNotIn(self.follower_a, self.test_class.follower.get(self.follower_b))
+
+	def test_follower_is_followed(self):
+		self.assertEqual(self.test_class.follower.is_followed(self.follower_a, self.follower_b), 0)
+		self.test_class.follower.insert(self.follower_a, self.follower_b)
+		self.assertEqual(self.test_class.follower.is_followed(self.follower_a, self.follower_b), 1)
+		self.test_class.follower.delete(self.follower_a, self.follower_b)
+
+class test_database_stranger(unittest.TestCase):
+	def setUp(self):
+		self.test_class = database.database()
+		self.to_user = -10000
+		self.from_user = -10001
+
+	def tearDown(self):
+		pass
+
+	def test_stranger_insert_get(self):
+		self.test_class.stranger.insert(to_user = self.to_user, from_user = self.from_user)
+		self.assertIn(self.from_user, self.test_class.stranger.get(self.to_user))
+		self.test_class.stranger.delete(to_user = self.to_user, from_user = self.from_user)
+
+	def test_stranger_delete(self):
+		self.test_class.stranger.insert(to_user = self.to_user, from_user = self.from_user)
+		self.test_class.stranger.delete(to_user = self.to_user, from_user = self.from_user)
+		self.assertNotIn(self.from_user, self.test_class.stranger.get(self.to_user))
+
+class test_database_bong(unittest.TestCase):
+	def setUp(self):
+		self.test_class = database.database()
+
+		self.date_calo = "2029-12-25"
+		self.data_calo_1 = {"openid": "calo_test", "startTime": "2029-12-25 0:0:0", "endTime": "2029-12-25 13:13:13", "type": 2, "subType": 2, "calories": 200}
+		self.data_calo_2 = {"openid": "calo_test", "startTime": "2029-12-25 13:33:33", "endTime": "2029-12-25 23:33:33", "type": 2, "subType": 2, "calories": 300}
+		self.data_calo_3 = {"openid": "calo_test", "startTime": "2029-12-26 0:0:0", "endTime": "2029-12-26 23:33:33", "type": 2, "subType": 2, "calories": 400}
+
+		self.date_sleep = "2029-12-25"
+		self.data_sleep_1 = {"openid": "calo_test", "startTime": "2029-12-24 0:0:0", "endTime": "2029-12-25 0:0:0", "type": 1, "subType": 2, "dsNum": 100, "lsNum": 200}
+		self.data_sleep_2 = {"openid": "calo_test", "startTime": "2029-12-24 13:33:33", "endTime": "2029-12-25 23:33:33", "type": 1, "subType": 2, "dsNum": 300, "lsNum": 400}
+		self.data_sleep_3 = {"openid": "calo_test", "startTime": "2029-12-25 0:0:0", "endTime": "2029-12-26 0:0:0", "type": 1, "subType": 2, "dsNum": 500, "lsNum": 600}
+
+	def tearDown(self):
+		self.test_class.bong.delete(self.data_calo_1["openid"])
+		self.test_class.bong.delete(self.data_sleep_1["openid"])
+
+	def test_bong_get_calories(self):
+		self.test_class.bong.insert(self.data_calo_1)
+		self.test_class.bong.insert(self.data_calo_2)
+		self.test_class.bong.insert(self.data_calo_3)
+
+		self.assertEqual(self.test_class.bong.get_calories(self.data_calo_1["openid"], self.date_calo), self.data_calo_1["calories"] + self.data_calo_2["calories"])
+
+	def test_bong_get_sleep(self):
+		self.test_class.bong.insert(self.data_sleep_1)
+		self.test_class.bong.insert(self.data_sleep_2)
+		self.test_class.bong.insert(self.data_sleep_3)
+
+		self.assertEqual(self.test_class.bong.get_sleep(self.data_sleep_1["openid"], self.date_sleep), {"dsleep": self.data_sleep_1["dsNum"] + self.data_sleep_2["dsNum"], "lsleep": self.data_sleep_1["lsNum"] + self.data_sleep_2["lsNum"]})
